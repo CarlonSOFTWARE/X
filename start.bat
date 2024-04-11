5954a6eecdc3f13e81e777f29f176bad6b88f819
Carlon <99050212+CarlonSOFTWARE@users.noreply.github.com>
2024-04-12 00:46:31 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Kütüphaneler yükleniyor...
+pip install pycryptodome pywin32 requests
+echo Kütüphaneler başarıyla yüklendi.
+echo ---------------------------------
+echo Python betiği çalıştırılıyor...
+python images\Developed-By-Carlon
